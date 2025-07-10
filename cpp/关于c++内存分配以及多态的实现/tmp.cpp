6022c969f8a00324aac28e565669b2cd4666f2b5
zhangbuda <59356783+tutuwu2019@users.noreply.github.com>
2025-07-10 15:08:01 +0800

    Create tmp.cpp

---
+++ cpp/关于c++内存分配以及多态的实现/tmp.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+double pi = 3.1415926;
+
+int inx;
+
+class Basic{
+public:
+    static float alias;
+    static int x1 = 10;      // 此处会在编译的时候报错
+    static void show1(){
+        std::cout<<"alias: "<<alias<<std::endl;
+    }
+    void show2(){
+        std::cout<<"alias*2:"<<alias*2<<std::endl;
+    }
+
+};
+
+float Basic::alias = 0.23333;
+// int Basic::x1 = 10;  //  不在类外定义 会报定义为使用错误  undefined reference to `Basic::x1'
+
+int main(){
+    Basic tmp;
+    
+    
+    Basic::show1();
+    tmp.show1();
+    tmp.show2();
+    std::cout<<"为初始化的静态成员变量 tmp.x1: "<<tmp.x1<<" Basic::x1: "<<Basic::x1<<std::endl;
+
+    std::cout<<"已经初始化的全局变量pi: "<<pi<<std::endl;
+    std::cout<<"未初始化的全局变量inx: "<<inx<<std::endl;
+    return 0;
+
+}
